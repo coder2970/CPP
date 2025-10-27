9884361014c44b7cafee0d5eb0fb18c917f06e50
coder2970 <13955275327@163.com>
2025-10-27 11:27:10 +0800

    string

--- 10-17 mystring/string.h
+++ 10-17 mystring/string.h
@@ -33,7 +33,7 @@ namespace mystring
             return _str + _size;
         }
 
-        string(const char* ="");
+        string(const char* str ="");
         ~string();
         void clear()
         {
