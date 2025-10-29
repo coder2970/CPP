77971ece80c2806327051df9b95b4db6d2fe634d
coder2970 <13955275327@163.com>
2025-10-29 10:51:27 +0800

    vector底层的部分简单实现

--- 10-27 myvector/test.cpp
+++ 10-27 myvector/test.cpp
@@ -2,24 +2,95 @@
 
 using namespace std;
 
+void func(const myvector::vector<int>& v)
+{
+    // const_iterator
+    myvector::vector<int>::const_iterator it = v.begin();
+    while(it != v.end())
+    {
+        cout<< *it ;
+        it++;
+    }
+    cout<<endl;
+}
 int main()
 {
     myvector::vector<int> v1;
     v1.push_back(1);
     v1.push_back(2);
+    // v1.push_back(20);
     v1.push_back(3);
     v1.push_back(4);
     v1.push_back(5);
+    // v1.push_back(50);
 
-    for(const auto& e : v1)
-    {
-        cout << e <<" ";
-    }
-    cout << endl;
+    // for(const auto& e : v1)
+    // {
+    //     cout << e <<" ";
+    // }
+    // cout << endl;
+    // v1.pop_back();
+    // v1.pop_back();
+    // v1.pop_back();
+    // v1.pop_back();
+    // v1.pop_back();
+    // v1.pop_back();
+    
+    // v1.insert(v1.begin(),10);
+    // v1.insert(v1.begin() + 2,20);
+    // v1.insert(v1.begin() + 2,30);
 
+    // v1.erase(v1.begin()+2);
+    // 删除偶数的正确写法 给erase返回值 记录每次删除后pos指向的位置
+    // auto it = v1.begin();
+    // while(it != v1.end())
+    // {
+    //     if(*it % 2 == 0)
+    //     {
+    //         it = v1.erase(it);
+    //     }
+    //     else{
+    //         it++;
+    //     }
+    // }
     for(size_t i = 0;i < v1.size();i++)
     {
         cout << v1[i] <<" ";
+    }
+    cout <<endl;
+
+    // myvector::vector<int> v2(v1);
+    // myvector::vector<int> v3;
+    // v3.resize(10,1);
+    // for(size_t i = 0;i < v3.size();i++)
+    // {
+    //     cout << v3[i] <<" ";
+    // }
+    // cout <<endl;
+    // for(size_t i = 0;i < v2.size();i++)
+    // {
+    //     cout << v2[i] <<" ";
+    // }
+    // cout <<endl;
+    // v3 = v1;
+    // for(size_t i = 0;i < v3.size();i++)
+    // {
+    //     cout << v3[i] <<" ";
+    // }
+    // cout <<endl;
+    // cout << v1._start << " " << v2._start <<endl;
+    // func(v1);
+
+    myvector::vector<string> v2;
+    v2.push_back("1111111111111111111111111111111111111111111");
+    v2.push_back("1111111111111111111111111111111111111111111");
+    v2.push_back("1111111111111111111111111111111111111111111");
+    v2.push_back("1111111111111111111111111111111111111111111");
+    v2.push_back("1111111111111111111111111111111111111111111");
+    v2.push_back("1111111111111111111111111111111111111111111");
+    for(size_t i = 0;i < v2.size();i++)
+    {
+        cout << v2[i] <<" ";
     }
     cout <<endl;
     return 0;
--- 10-27 myvector/vector.h
+++ 10-27 myvector/vector.h
@@ -9,6 +9,8 @@ namespace myvector
     {
     public:
         typedef T *iterator;
+        typedef const T* const_iterator;
+
         iterator begin() 
         {
             return _start;
@@ -18,9 +20,29 @@ namespace myvector
         {
             return _finish;
         }
-        vector()
-            : _start(nullptr), _finish(nullptr), _end_of_storage(nullptr)
+
+        const_iterator begin() const
         {
+            return _start;
+        }
+
+        const_iterator end() const
+        {
+            return _finish;
+        }
+
+        vector()
+        {
+        }
+
+        // 拷贝构造
+        vector(const vector<T>& v)
+        {
+            reserve(v.size());
+            for(auto e : v)
+            {
+                push_back(e);
+            }
         }
 
         ~vector()
@@ -29,14 +51,55 @@ namespace myvector
             _start = _finish = _end_of_storage = nullptr;
         }
 
+        // 赋值重载
+        vector<T>& operator=(const vector<T>& v)
+        {
+            if(this != &v)
+            {
+                delete[] _start;
+                _start = _finish = _end_of_storage = nullptr;
+                reserve(v.size());
+                for(const auto& e: v)
+                {
+                    push_back(e);
+                }
+            }
+            return *this;
+        }
+
+        void resize(size_t n,const T& x = T())
+        {
+            if(n < size())
+            {
+                _finish = _start + n;
+            }
+            else
+            {
+                reserve(n);
+                while(_finish != _start + n)
+                {
+                    *_finish = x;
+                    _finish ++;
+                }
+            }
+        }
+
         void reserve(size_t n)
         {
             if(n > capacity())
             {
+                size_t oldsize = size();
                 T* tmp = new T[n];
-                memcpy(tmp,_start,sizeof(T)*size());
+                // memcpy 也存在浅拷贝的问题
+                // memcpy(tmp,_start,sizeof(T)*size());
+                for(size_t i = 0;i < oldsize;i++)
+                {
+                    tmp[i] = _start[i];
+                }
                 delete[] _start;
-                _finish = tmp + size();
+
+
+                _finish = tmp + oldsize;
                 _start = tmp;
                 // error
                 // _start = tmp;
@@ -50,6 +113,13 @@ namespace myvector
             assert(i < size());
             return _start[i];
         }
+
+        const T& operator[](size_t i) const
+        {
+            assert(i < size());
+            return _start[i];
+        }
+
         size_t size() const
         {
             return _finish - _start;
@@ -71,10 +141,56 @@ namespace myvector
             _finish ++;
         }
 
+        void pop_back()
+        {
+            assert(_finish > _start);
+            --_finish;
+        }
+
+        void insert(iterator pos,const T& x)
+        {
+            assert(pos <= _finish);
+            assert(pos >= _start);
+
+            // 扩容时注意迭代器失效问题
+            if(_finish == _end_of_storage)
+            {
+                size_t len = pos - _start;
+                reserve(capacity() == 0 ? 4 : capacity() * 2);
+                pos = _start + len; // 让pos 与 start对应
+            }
+
+            // 挪动数据
+            iterator it = _finish - 1;
+            while(it >= pos)
+            {
+                *(it + 1) = *(it);
+                it --;
+            }
+
+            *pos = x;
+            ++_finish;
+        }
+
+        iterator erase(iterator pos)
+        {
+            assert(pos <= _finish);
+            assert(pos >= _start);
+
+            iterator it = pos + 1;
+            while(it <= _finish)
+            {
+                *(it-1) = *(it);
+                it ++;
+            }
+            --_finish;
+            return pos;
+        }
+
     private:
-        iterator _start;          // 指向第一个元素
-        iterator _finish;         // 指向最后一个元素的下一个位置
-        iterator _end_of_storage; // 总容量
+        iterator _start = nullptr;          // 指向第一个元素
+        iterator _finish = nullptr;         // 指向最后一个元素的下一个位置
+        iterator _end_of_storage = nullptr; // 总容量
     };
 
 }
