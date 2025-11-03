00f99e97282a00e71fbb32145fe8df7e0dcedea6
coder2970 <13955275327@163.com>
2025-11-03 22:09:33 +0800

    list基本功能与迭代器完善

--- 10-29 mylist/list.h
+++ 10-29 mylist/list.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <assert.h>
+#include <iostream>
 
 namespace mylist
 {
@@ -9,9 +11,156 @@ namespace mylist
         list_node<T> *_prev;
         T _data;
 
-        list_node(const T& x = T())
-            :_next(nullptr), _prev(nullptr), _data(x)
+        list_node(const T &x = T())
+            : _next(nullptr), _prev(nullptr), _data(x)
         {
+        }
+    };
+
+    // // 普通迭代器的实现
+    // template <class T>
+    // struct list_iterator
+    // {
+    //     typedef list_node<T> Node;
+
+    //     Node *_node;
+
+    //     list_iterator(Node *node)
+    //         : _node(node)
+    //     {
+    //     }
+
+    //     // *it
+    //     T &operator*()
+    //     {
+    //         return _node->_data;
+    //     }
+
+    //     // it->data  --> it.operator->()->data
+    //     T *operator->()
+    //     {
+    //         return &_node->_data;
+    //     }
+
+    //     // it++
+    //     list_iterator<T> &operator++()
+    //     {
+    //         _node = _node->_next;
+    //         return *this;
+    //     }
+
+    //     bool operator!=(const list_iterator<T> &it)
+    //     {
+    //         return _node != it._node;
+    //     }
+
+    //     bool operator==(const list_iterator<T> &it)
+    //     {
+    //         return _node == it._node;
+    //     }
+    // };
+
+    // // const 迭代器的实现
+    // template <class T>
+    // struct list_const_iterator
+    // {
+    //     typedef list_node<T> Node;
+
+    //     Node *_node;
+
+    //     list_const_iterator(Node *node)
+    //         : _node(node)
+    //     {
+    //     }
+
+    //     // == != ++ * ->
+
+    //     bool operator==(const list_const_iterator<T> &it)
+    //     {
+    //         return _node == it._node;
+    //     }
+
+    //     bool operator!=(const list_const_iterator<T> &it)
+    //     {
+    //         return _node != it._node;
+    //     }
+
+    //     list_const_iterator<T> operator++()
+    //     {
+    //         _node = _node->_next;
+    //         return *this;
+    //     }
+
+    //     const T &operator*()
+    //     {
+    //         return _node->_data;
+    //     }
+
+    //     const T *operator->()
+    //     {
+    //         return &_node->_data;
+    //     }
+    // };
+
+    // 优化：能不能用模板同时实现普通迭代器和const迭代器
+    template <class T, class Ref, class Ptr> // reference pointer
+    struct list_iterator
+    {
+        typedef list_node<T> Node;
+        typedef list_iterator<T, Ref, Ptr> Self;
+        Node *_node;
+
+        list_iterator(Node *node)
+            : _node(node)
+        {
+        }
+
+        // *it
+        Ref operator*()
+        {
+            return _node->_data;
+        }
+
+        // it->data  --> it.operator->()->data
+        Ptr operator->()
+        {
+            return &_node->_data;
+        }
+
+        // it++
+        Self &operator++()
+        {
+            _node = _node->_next;
+            return *this;
+        }
+        Self operator++(int)
+        {
+            Self tmp(*this);
+            _node = _node->_next;
+            return tmp;
+        }
+
+        Self &operator--()
+        {
+            _node = _node->_prev;
+            return *this;
+        }
+
+        Self operator--(int)
+        {
+            Self tmp(*this);
+            _node = _node->_prev;
+            return tmp;
+        }
+
+        bool operator!=(const Self &it) const
+        {
+            return _node != it._node;
+        }
+
+        bool operator==(const Self &it) const
+        {
+            return _node == it._node;
         }
     };
 
@@ -21,26 +170,151 @@ namespace mylist
         typedef list_node<T> Node;
 
     public:
-        list()
+        // typedef list_iterator<T> iterator;
+        // typedef list_const_iterator<T> const_iterator;
+        typedef list_iterator<T, T &, T *> iterator;
+        typedef list_iterator<T, const T &, const T *> const_iterator;
+
+        void empty_init()
         {
             _head = new Node;
             _head->_next = _head;
             _head->_prev = _head;
+
+            _size = 0;
         }
 
-        void push_back(const T& x)
+        list()
         {
-            Node* newnode = new Node(x);
+            empty_init();
+        }
 
-            Node* tail = _head->_prev; // 找到尾节点
-            tail->_next = newnode;
-            newnode->_prev = tail;
+        list(const list<T> &lt)
+        {
+            empty_init();
+            for (auto e : lt)
+            {
+                push_back(e);
+            }
+        }
+        void clear()
+        {
+            iterator it = begin();
+            while (it != end())
+            {
+                it = erase(it);
+            }
+        }
 
-            newnode->_next = _head;
-            _head->_prev = newnode;
+        ~list()
+        {
+            clear();
+            delete _head;
+            _head = nullptr;
+        }
+
+        iterator begin()
+        {
+            iterator it(_head->_next);
+            return it;
+        }
+
+        iterator end()
+        {
+            iterator it(_head);
+            return it;
+        }
+        const_iterator begin() const
+        {
+            const_iterator it(_head->_next);
+            return it;
+        }
+
+        const_iterator end() const
+        {
+            const_iterator it(_head);
+            return it;
+        }
+
+        void swap(list<T> &lt)
+        {
+            std::swap(_head, lt._head);
+            std::swap(_size, lt._size);
+        }
+
+        // 注意这里传参不能传引用
+        list<T> &operator=(list<T> lt)
+        {
+            swap(lt);
+            return *this;
+        }
+        size_t size() const
+        {
+            return _size;
+        }
+
+        void push_back(const T &x)
+        {
+            // Node *newnode = new Node(x);
+
+            // Node *tail = _head->_prev; // 找到尾节点
+            // tail->_next = newnode;
+            // newnode->_prev = tail;
+
+            // newnode->_next = _head;
+            // _head->_prev = newnode;
+
+            insert(end(), x);
+        }
+
+        void insert(iterator pos, const T &x)
+        {
+            Node *cur = pos._node;
+            Node *prev = cur->_prev;
+
+            Node *newnode = new Node(x);
+
+            prev->_next = newnode;
+            newnode->_prev = prev;
+
+            newnode->_next = cur;
+            cur->_prev = newnode;
+
+            ++_size;
+        }
+        iterator erase(iterator pos)
+        {
+            assert(pos != end());
+            Node *cur = pos._node;
+            Node *nextnode = cur->_next;
+            Node *prevnode = cur->_prev;
+
+            prevnode->_next = nextnode;
+            nextnode->_prev = prevnode;
+
+            delete cur;
+            --_size;
+            // 处理迭代器失效问题
+            return iterator(nextnode);
+        }
+
+        void push_front(const T &x)
+        {
+            insert(begin(), x);
+        }
+
+        void pop_back()
+        {
+            erase(--end());
+        }
+
+        void pop_front()
+        {
+            erase(begin());
         }
 
     private:
         Node *_head;
+        size_t _size;
     };
 }
--- 10-29 mylist/test.cpp
+++ 10-29 mylist/test.cpp
@@ -1,16 +1,100 @@
 #include "list.h"
+#include <iostream>
+
+using namespace std;
+
+struct A
+{
+    A(int a1 = 1, int a2 = 1)
+        : _a1(a1), _a2(a2)
+    {
+    }
+
+    int _a1;
+    int _a2;
+};
+
+void test_mylist2()
+{
+    mylist::list<A> lt2;
+    lt2.push_back({1, 1});
+    lt2.push_back({2, 2});
+
+    mylist::list<A>::iterator it = lt2.begin();
+
+    while (it != lt2.end())
+    {
+        cout << it->_a1 << " " << it->_a2 << " ";
+        ++it;
+    }
+    cout << endl;
+}
+
+void Print(const mylist::list<int> lt)
+{
+    mylist::list<int>::const_iterator it = lt.begin();
+    while (it != lt.end())
+    {
+        cout << *it << " ";
+        ++it;
+    }
+    cout << endl;
+}
+
 void test_mylist()
 {
     mylist::list<int> lt;
     lt.push_back(1);
+    lt.push_back(2);
+    lt.push_back(3);
+    lt.push_back(4);
+    lt.push_back(5);
+    mylist::list<int>::iterator it = lt.begin();
+    while (it != lt.end())
+    {
+        cout << *it << " ";
+        ++it;
+    }
+    cout << endl;
+
+    for (auto &e : lt)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    Print(lt);
+}
+void test_mylist3()
+{
+    mylist::list<int> lt;
     lt.push_back(1);
-    lt.push_back(1);
-    lt.push_back(1);
-    lt.push_back(1);
-    
+    lt.push_back(2);
+    lt.push_back(3);
+    lt.push_front(4);
+    lt.push_front(5);
+    lt.push_front(6);
+
+    lt.pop_back();
+    lt.pop_back();
+    lt.pop_front();
+    lt.pop_front();
+
+    for (auto &e : lt)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+    mylist::list<int> lt2;
+    lt2 = lt;
+    for (auto &e : lt)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
 }
 int main()
 {
-    test_mylist();
+    test_mylist3();
     return 0;
 }
