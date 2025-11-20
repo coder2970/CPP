2ab0dbb86b6c53cc8af5b06e1aa47519ec2a5915
coder2970 <13955275327@163.com>
2025-11-20 10:26:06 +0800

    二叉搜索树的删除

--- 11-17 BinarySearchTree/BinarySearchTree.h
+++ 11-17 BinarySearchTree/BinarySearchTree.h
@@ -107,6 +107,96 @@ public:
         return false;
     }
 
+    bool Erase(const K &key)
+    {
+        Node *parent = nullptr;
+        Node *cur = _root;
+        while (cur)
+        {
+            if (key > cur->_key)
+            {
+                parent = cur;
+                cur = cur->_right;
+            }
+            else if (key < cur->_key)
+            {
+                parent = cur;
+                cur = cur->_left;
+            }
+            else
+            {
+                // 找到该节点 cur即为要删除结点
+                // 开始删除
+                if (cur->_left == nullptr) // 左子树为空
+                {
+                    // cur->_right != nullptr
+                    // 即下面的操作都要想 要把cur->right给谁
+                    if (cur == _root)
+                    {
+                        _root = cur->_right;
+                    }
+                    else
+                    {
+                        if (parent->_left == cur)
+                        {
+                            parent->_left = cur->_right;
+                        }
+                        else
+                        {
+                            parent->_right = cur->_right;
+                        }
+                    }
+                    delete cur;
+                }
+                else if (cur->_right == nullptr) // 右子树为空
+                {
+                    if (cur == _root)
+                    {
+                        _root = cur->_left;
+                    }
+                    else
+                    {
+                        if (parent->_left == cur)
+                        {
+                            parent->_left = cur->_left;
+                        }
+                        else
+                        {
+                            parent->_right = cur->_left;
+                        }
+                    }
+                    delete cur;
+                }
+                else // 左右子树都不为空
+                {
+                    // 找右子树的最左节点(最小节点)代替
+                    Node *replace = cur->_right;
+                    Node *replaceParent = cur;
+                    while (replace->_left)
+                    {
+                        replaceParent = replace;
+                        replace = replace->_left;
+                    }
+                    swap(replace->_key, cur->_key);
+
+                    // 此时要删除的是这个最左节点，这个节点的左子树一定为空
+                    // 但是要考虑这个节点是父亲的左节点还是右节点
+                    if (replaceParent->_left == replace)
+                    {
+                        replaceParent->_left = replace->_right;
+                    }
+                    else
+                    {
+                        replaceParent->_right = replace->_right;
+                    }
+                    delete replace;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
 private:
     Node *_root = nullptr;
 };
@@ -121,8 +211,22 @@ void testBSTree()
     }
     tree.InOrder();
 
-    tree.Insert(50);
-    tree.InOrder();
-    tree.Find(50);
-    tree.Find(2000);
+    // tree.Insert(50);
+    // tree.InOrder();
+    // tree.Find(50);
+    // tree.Find(2000);
+    // tree.Erase(8);
+    // tree.InOrder();
+    // tree.Erase(3);
+    // tree.InOrder();
+    // tree.Erase(14);
+    // tree.InOrder();
+    // // 左为空
+    // tree.Erase(6);
+    // tree.InOrder();
+    for (auto e : a)
+    {
+        tree.Erase(e);
+        tree.InOrder();
+    }
 }
