549449a39a0bf1f6c2ca2916c1df072cab5e66c1
coder2970 <13955275327@163.com>
2025-11-30 21:05:05 +0800

    归并排序

--- Sort/test.cpp
+++ Sort/test.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 #include <time.h>
 #include <cstdlib>
+
 using namespace std;
+// 快速排序
 int getRandom(vector<int> &nums, int left, int right)
 {
     return nums[(rand() % (right - left + 1)) + left];
 }
-// 快速排序
+
 void quickSort(vector<int> &nums, int l, int r)
 {
     if (l >= r)
         return;
     int key = getRandom(nums, l, r);
-    int left = l - 1, right = r + 1, i = l;
+    int left = l - 1, right = r + 1, i = l; // 注意i不能从0开始，应该从传入区间的端点处开始
     while (i < right)
     {
         if (key > nums[i])
@@ -28,6 +30,31 @@ void quickSort(vector<int> &nums, int l, int r)
     }
 }
 
+// 归并排序
+void mergeSort(vector<int> &nums, int left, int right)
+{
+    if (left >= right)
+        return;
+    // 1.取数组中点
+    int middle = (left + right) >> 1;
+    // 2.排序左右两边
+    // [left, middle] [middle + 1, right]
+    mergeSort(nums, left, middle);
+    mergeSort(nums, middle + 1, right);
+    // 3.合并有序数组
+    vector<int> tmp(right - left + 1);
+    int cur1 = left, cur2 = middle + 1, i = 0;
+    while (cur1 <= middle && cur2 <= right)
+        tmp[i++] = nums[cur1] < nums[cur2] ? nums[cur1++] : nums[cur2++];
+    while (cur1 <= middle)
+        tmp[i++] = nums[cur1++];
+    while (cur2 <= right)
+        tmp[i++] = nums[cur2++];
+    // 4.还原
+    for (int k = left; k <= right; k++)
+        nums[k] = tmp[k - left];
+}
+
 int main()
 {
     srand(time(NULL));
@@ -37,7 +64,8 @@ int main()
         cout << e << " ";
     cout << endl;
 
-    quickSort(nums, 0, n - 1);
+    // quickSort(nums, 0, n - 1);
+    mergeSort(nums, 0, n - 1);
 
     for (auto e : nums)
         cout << e << " ";
