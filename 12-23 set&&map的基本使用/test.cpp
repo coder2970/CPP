ead5566d1e96d2c23b4a587a6a524e002ed221b5
coder2970 <13955275327@163.com>
2025-12-24 16:25:33 +0800

    map中insert的补充,operator[],multimap

--- 12-23 set&&map的基本使用/test.cpp
+++ 12-23 set&&map的基本使用/test.cpp
@@ -150,35 +150,125 @@
 // }
 #include <string>
 #include <map>
+// int main()
+// {
+//     // 定义简单的字典
+//     // insert
+//     std::map<std::string, std::string> dict;
+
+//     // 有名对象
+//     std::pair<std::string, std::string> kv1("sort", "排序");
+//     dict.insert(kv1);
+
+//     // 匿名对象
+//     dict.insert(std::pair<std::string, std::string>("apple", "苹果"));
+
+//     // c++98:make_pair
+//     dict.insert(std::make_pair("red", "红色"));
+
+//     // c++11:不是initializer_lists
+//     // 多参数构造函数的隐式类型转换
+//     // pair(const T1&a,const T2&b):first(a),second(b) {}
+//     // 过程 : {"house", "房子"} -> pair对象 -> dict.insert(pair对象)
+//     dict.insert({"house", "房子"});
+//     //	// 已经存在的值再次插入会失败
+//     //	dict.insert({ "house" , "屋子" });
+//     // 迭代器
+//     auto it = dict.begin();
+//     while (it != dict.end())
+//     {
+//         // 实际上是: it.operator->()->first 编译器省略了一个(优化)
+//         std::cout << it->first << " " << it->second << std::endl;
+//         it++;
+//     }
+//     return 0;
+// }
+
+// int main()
+//{
+//	// 外层{}:initializer_lists
+//	// 内层{}:多参数构造支持隐式类型转换,转化为pair类型
+//	std::map<std::string, std::string > dict = { {"sort", "排序" } ,{"apple", "苹果"} ,{"house", "房子"} };
+//
+//	// 这里最好加上&,否则拷贝代价很大
+//	for (auto& kv : dict)
+//	{
+//		// kv相当于*it的别名
+//		std::cout << kv.first << kv.second << std::endl;
+//	}
+//	return 0;
+// }
+
+// int main()
+//{
+//	// 统计次数
+//	std::string arr[] = { "苹果","西瓜","苹果","西瓜","苹果","苹果","西瓜","苹果","香蕉","苹果","香蕉" };
+//	std::map<std::string, int> count;
+//	for (auto& e : arr)
+//	{
+//		//auto it = count.find(e);
+//		//if (it == count.end())
+//		//{
+//		//	// 第一次出现
+//		//	count.insert({ e, 1 });
+//		//}
+//		//else
+//		//{
+//		//	it->second++;
+//		//}
+//
+//		// []
+//		count[e]++;
+//	}
+//
+//	std::pair< std::map<std::string, int>::iterator, bool> ret1 = count.insert({ "苹果",1 });
+//	std::pair< std::map<std::string, int>::iterator, bool> ret2 = count.insert({ "草莓",1 });
+//
+//	//V& operator[](const K& key)
+//	//{
+//	//	// 直接插入Value的默认构造
+//	//	// 自定义类型调用其对应的默认构造(vector-空数组,string-空字符串
+//	//	// 内置类型也有其默认构造(int-0, double-0.0,ptr-nullptr)
+//	//	pair<iteraotr, bool> ret = insert(key, V());
+//
+//	//	// 无论insert成功失败,该迭代器都是指向key的迭代器
+//
+//	//	// ret.first:取pair中的迭代器,再由迭代器取key对应的value(通过迭代器中节点的指针访问)
+//	//	return ret.first->second;
+//	//}
+//
+//	for (auto& e : count)
+//		std::cout << e.first << e.second << std::endl;
+//	return 0;
+// }
+
+// int main()
+//{
+//	std::map<std::string, std::string> dict;
+//
+//	// 1.插入
+//	dict["left"];
+//
+//	// 2.插入 + 修改
+//	dict["right"] = "右边";
+//
+//	// 3.修改
+//	dict["left"] = "左边";
+//
+//	// 4.查找
+//	std::cout << dict["left"] << std::endl;
+//	return 0;
+// }
+
 int main()
 {
-    // 定义简单的字典
-    // insert
-    std::map<std::string, std::string> dict;
+    std::multimap<std::string, std::string> dict;
+    dict.insert({"apple", "苹果"});
+    dict.insert({"sort", "排序"});
 
-    // 有名对象
-    std::pair<std::string, std::string> kv1("sort", "排序");
-    dict.insert(kv1);
+    dict.insert({"apple", "xxx"});
 
-    // 匿名对象
-    dict.insert(std::pair<std::string, std::string>("apple", "苹果"));
-
-    // c++98:make_pair
-    dict.insert(std::make_pair("red", "红色"));
-
-    // c++11:不是initializer_lists
-    // 多参数构造函数的隐式类型转换
-    // pair(const T1&a,const T2&b):first(a),second(b) {}
-    // 过程 : {"house", "房子"} -> pair对象 -> dict.insert(pair对象)
-    dict.insert({"house", "房子"});
-
-    // 迭代器
-    auto it = dict.begin();
-    while (it != dict.end())
-    {
-        // 实际上是: it.operator->()->first 编译器省略了一个(优化)
-        std::cout << it->first << " " << it->second << std::endl;
-        it++;
-    }
+    for (auto &e : dict)
+        std::cout << e.first << e.second << std::endl;
     return 0;
 }
